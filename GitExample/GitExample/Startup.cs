06c6bbeb2ba27ae54045bb53821bd0f99e3f58d0
Chasten Warwick <cwarwick@servpronet.com>
2015-02-24 09:45:09 -0600

    Added base project

---
+++ GitExample/GitExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitExample.Startup))]
+namespace GitExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
