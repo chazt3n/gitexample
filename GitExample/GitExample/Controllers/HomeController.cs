903ff7f73f85a2cd3acced7e3c60aba5ec30d7a1
Chasten Warwick <cwarwick@servpronet.com>
2015-02-24 09:53:16 -0600

    introducing bug into productions

--- GitExample/GitExample/Controllers/HomeController.cs
+++ GitExample/GitExample/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace GitExample.Controllers
 {
@@ -17,7 +13,8 @@ namespace GitExample.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            //Looks like someone introduced a bug!
+            return View("Error");
         }
 
         public ActionResult Contact()
