fe34f06472ab7b7f29f27c4824685cd7fbb172cc
Chasten Warwick <cwarwick@servpronet.com>
2015-02-24 09:53:16 -0600

    introducing bug into productions

--- GitExample/GitExample/Controllers/HomeController.cs
+++ GitExample/GitExample/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace GitExample.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View("Error");
         }
 
         public ActionResult Contact()
